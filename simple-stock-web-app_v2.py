1af17d76a257ce860a56ce4fa3f0e80b0260cc39
ericttran3 <ericttran3@gmail.com>
2021-06-17 22:48:30 -0700

    updated app

--- simple-stock-web-app_v2.py
+++ simple-stock-web-app_v2.py
@@ -51,7 +51,7 @@ def main():
     # App title
     st.markdown('''
     # Simple Stock App
-    A stock app to research stocks and track performance over time. This tool is used for educational purposes only and does not provide any financial investment advice. Do your own research and due dilligence before buying or selling stocks. Invest at your own risk. This data is updated every 1 hour and may be delayed.
+    A stock app to research stocks and track performance over time. This tool is used for educational purposes only and does not provide any financial investment advice. Do your own research and due dilligence before buying or selling stocks. Invest at your own risk. This data is updated every hour and may be delayed.
     ''')
 
     # Sidebar
